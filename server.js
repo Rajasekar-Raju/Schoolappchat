5729790e0ab6cb823508a3fd0fbe7202932bf52c
moaj257 <moaj257@gmail.com>
2020-10-26 09:31:09 +0530

    Heroku Deployment

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const io = require("socket.io");
-const {Pool} = require('pg');
+// const {Pool} = require('pg');
 const bodyParser = require("body-parser");
 const app = express();
 const server = http.createServer(app);
 const socket = io(server);
 
-const DB_USER = '';
-const DB_PASSWORD = '';
-const DB_HOST = '';
-const DB_PORT = '';
-const DB_DATABASE = '';
-const CONNECTION_STRING = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+// const DB_USER = '';
+// const DB_PASSWORD = '';
+// const DB_HOST = '';
+// const DB_PORT = '';
+// const DB_DATABASE = '';
+// const CONNECTION_STRING = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-const pool = new Pool({
-  connectionString: CONNECTION_STRING,
-  ssl: true,
-});
+// const pool = new Pool({
+//   connectionString: CONNECTION_STRING,
+//   ssl: true,
+// });
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -32,15 +32,15 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req, res) => res.sendFile(INDEX, { root: __dirname }));
-app.post('/getmessages', (req, res) => {
-  const {sender, receiver} = req.body;
-  pool.query(`SELECT * FROM messages WHERE (sender = "${sender}" AND receiver = "${receiver}") OR (sender = "${receiver}" AND receiver ="${sender}")`, (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows);
-  });
-});
+// app.post('/getmessages', (req, res) => {
+//   const {sender, receiver} = req.body;
+//   pool.query(`SELECT * FROM messages WHERE (sender = "${sender}" AND receiver = "${receiver}") OR (sender = "${receiver}" AND receiver ="${sender}")`, (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     res.status(200).json(results.rows);
+//   });
+// });
 
 server.listen(PORT, () => console.log(`Connected in ${PORT}`));
 
@@ -55,11 +55,11 @@ socket.on('connection', (data) => {
     const {sender, receiver, message} = res;
     var socketId = users[receiver];
     socket.to(socketId).emit("new_message", res);
-    pool.query(`INSERT INTO messages (sender, receiver, message) VALUES ("${sender}", "${receiver}", "${message}")`, (error, results) => {
-      if (error) {
-        throw error
-      }
-    });
+    // pool.query(`INSERT INTO messages (sender, receiver, message) VALUES ("${sender}", "${receiver}", "${message}")`, (error, results) => {
+    //   if (error) {
+    //     throw error
+    //   }
+    // });
   });
 });
 
